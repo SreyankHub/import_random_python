9c487a6b725ee1ac65672934c7ae8ae718a94b77
SreyankHub <SreyankBose@gmail.com>
2022-03-05 23:02:38 -0800

    Import Random Project

---
+++ main.py
@@ -0,0 +1,35 @@
+#Choice Maker Game
+
+import random
+
+#list to store item
+Game1 = ['cat','dog','rabbit','horse']
+
+#list to store item
+Game2 = ['cat','dog','rabbit','horse']
+
+#random use to choose an item
+print(random.choice(Game1))
+print(random.choice(Game2))
+
+#input to ask the user does he/she like this choice
+like = input('do u like this choice?')
+
+#to print the answer
+print('your answer is '+like)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
